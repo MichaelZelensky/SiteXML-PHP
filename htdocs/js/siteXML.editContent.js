9c9e1cbe32a00e123600fef19d85572e559c027b
michaelzelensky <mz1983289>
2015-05-06 13:32:07 +0200

    editContent.js - save on blur

--- htdocs/js/siteXML.editContent.js
+++ htdocs/js/siteXML.editContent.js
@@ -13,25 +13,32 @@ $(function() {
         $(content[i]).on('click', function () {
             $(this).prop('contenteditable', true).focus();
         });
-        $(content[i]).on('input', function () {
-            //send content back to be saved
-            var content = this.innerHTML,
-                cid = this.getAttribute('cid'),
-                cidS = 'cid' + cid;
+        $(content[i])
+            .on('input blur', function (e) {
+                //send content back to be saved
+                var content = this.innerHTML,
+                    cid = this.getAttribute('cid'),
+                    cidS = 'cid' + cid;
 
-            if (window.siteXMLsaveTimeout[cidS]) {
-                clearTimeout(window.siteXMLsaveTimeout[cidS]);
-            }
-            window.siteXMLsaveTimeout[cidS] = setTimeout(function(){
-                $.ajax({
-                    method : 'POST',
-                    url : '/',
-                    data : {
-                        cid : cid,
-                        content : content
-                    }
-                });
-            }, 3000);
-        });
+                if (window.siteXMLsaveTimeout[cidS]) {
+                    clearTimeout(window.siteXMLsaveTimeout[cidS]);
+                }
+
+                function save () {
+                    $.ajax({
+                        method : 'POST',
+                        url : '/',
+                        data : {
+                            cid : cid,
+                            content : content
+                        }
+                    });
+                }
+                if (e.type === 'input') {
+                    window.siteXMLsaveTimeout[cidS] = setTimeout(save, 3000);
+                } else {
+                    save();
+                }
+            });
     }
 });
